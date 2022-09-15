85da48a9126ee5eb8adea8001cee06ecf3afc148
dylanrattai <817682@mystma.org>
2022-09-15 15:14:26 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + b + c + d + e
+
+print(topFiveMovies("Top Gun Maverick", "Baby Driver", "End of Watch", "Blackhawk Down", "Top Gun"))
